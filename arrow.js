43817ac456a480c9e8729db1df5dbea7b0e3ce6c
SayedaNoor <96574987+SayedaNoor@users.noreply.github.com>
2022-09-30 20:28:50 +0600

    done

---
+++ arrow.js
@@ -0,0 +1,9 @@
+const type1=(num1,num2) => num1+num2;
+const sum=type1(12,12);
+console.log("Hello Jaana");
+console.log(sum);
+const type2=()=> 'noor'
+const name= type2();
+console.log(name);
+//bah bah good :p
+//Thanks <3
---
+++ spread.js
@@ -0,0 +1,6 @@
+const numbers=[12,13,14,15,16,17];
+console.log(numbers);
+console.log(...numbers);
+const max=Math.max(12,13,14,15,16,17);
+const maxInArray=Math.max(...numbers);
+console.log(max,maxInArray);
